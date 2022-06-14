1510b908893335b4fbed4a98810e5f7ec3579665
AnoshMathew <anosh.mathew99@gmail.com>
2022-06-14 14:49:26 +0530

    commit message

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://work.phpwebsites.in/fishing/api/";
+const baseUrl = "https://work.phpwebsites.in/fishing/api/";
 const Url = {
   loginurl: baseUrl + "login",
   registerurl : baseUrl + "register",
